c595a6d096b0a8968a6bda299de31fe550a650e0
lightsing <light.tsing@gmail.com>
2016-08-09 22:24:38 +0800

    Configure Files

---
+++ client_config.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+from util import Menu
+
+__title__ = 'Secure Cli-Chatting v0.1'
+__main_menu__ = ['Connect to Server','Exit']
+
+mainMenu = Menu(__main_menu__,title = __title__)
