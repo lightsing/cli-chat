070f93ce255ca30aeddbda70fa33953a38faceca
lightsing <light.tsing@gmail.com>
2016-08-10 13:05:19 +0800

    Add Last Seen Timestamp

--- util/user.py
+++ util/user.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-
 __author__ = 'lightsing'
-
+import time
 class User(object) :
     def __init__(self, username, password) :
         self.username = username
         self.password = password
         self.isOnline = False
+        self.lastseen = time.time()
         self.token = ''
         self.contacts = []
         self.pubkey = None
