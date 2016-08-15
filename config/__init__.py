4275c06ad93cbfaab2ba9fa852cc6f1a852de38d
lightsing <light.tsing@gmail.com>
2016-08-15 22:49:34 +0800

    Upgrade Configure File

--- config/__init__.py
+++ config/__init__.py
@@ -5,14 +5,12 @@ import ssl
 
 __version__ = 0.1
 
-PORT = 9999
-ADDR = ('127.0.0.1', port)
+PORT = 8000
+ADDR = ('127.0.0.1', PORT)
 
-SSL_VERSION = ssl.PROTOCOL_TLSv1.2
+SSL_VERSION = ssl.PROTOCOL_TLSv1
 SSL_CIPHERS = None
 
-SSL_CERT_REQS = ssl.CERT_REQUIRED
-# SSL_CERT_REQS = ssl.CERT_OPTIONAL
-SSL_KEY_FILE = 'key.pem'
+SSL_CERT_REQS = ssl.CERT_OPTIONAL
+SSL_KEY_FILE = 'server.pem'
 SSL_CERT_FILE = 'server.crt'
-SSL_CA_CERT = 'ca.crt'
